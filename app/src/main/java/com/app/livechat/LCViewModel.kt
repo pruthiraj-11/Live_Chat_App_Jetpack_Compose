d49c1deaac43e43aed060ace27d47a3c377a27a2
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2024-03-13 22:07:28 +0530

    LC

--- app/src/main/java/com/app/livechat/LCViewModel.kt
+++ app/src/main/java/com/app/livechat/LCViewModel.kt
@@ -1,6 +1,8 @@
 package com.app.livechat
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.app.livechat.data.Event
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.lang.Exception
@@ -13,6 +15,8 @@ class LCViewModel @Inject constructor(
     init {
 
     }
+    var inProcess= mutableStateOf(false)
+    val eventMutableState= mutableStateOf<Event<String>?>(null)
     fun signup(name:String,phone:String,email:String,password:String) {
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
             if (it.isSuccessful) {
